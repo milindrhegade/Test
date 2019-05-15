6c1ec6c4e1dad4e23614c933e9477f242a43fa70
PARESH <PARESH@PARESH-PC>
2019-05-15 12:06:53 +0530

    new file added

---
+++ milinddemo/src/milinddemo/Addition.java
@@ -0,0 +1,12 @@
+package milinddemo;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("changes made by paresh");
+	}
+
+}
